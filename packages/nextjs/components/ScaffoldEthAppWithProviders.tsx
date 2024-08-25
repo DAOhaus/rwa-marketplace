a1f6ba0b5206b4dadaaaec3ab2482b956518ff13
spyros-zikos <zikossp@gmail.com>
2024-08-25 23:23:37 +0300

    start over

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -1,40 +1,17 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { rainbowWeb3AuthConnector } from "./Web3AuthProvider";
-import { RainbowKitProvider, darkTheme, getDefaultConfig, lightTheme } from "@rainbow-me/rainbowkit";
-import { metaMaskWallet, rainbowWallet } from "@rainbow-me/rainbowkit/wallets";
+import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useTheme } from "next-themes";
 import { Toaster } from "react-hot-toast";
-import { WagmiProvider, http } from "wagmi";
-import { mainnet, polygon, sepolia } from "wagmi/chains";
+import { WagmiProvider } from "wagmi";
 import { Footer } from "~~/components/Footer";
 import { Header } from "~~/components/Header";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
 import { ProgressBar } from "~~/components/scaffold-eth/ProgressBar";
 import { useInitializeNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
-
-// import { wagmiConfig } from "~~/services/web3/wagmiConfig";
-
-const config = getDefaultConfig({
-  appName: "My RainbowKit App",
-  projectId: "04309ed1007e77d1f119b85205bb779d",
-  chains: [mainnet, sepolia, polygon],
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-    [polygon.id]: http(),
-  },
-  wallets: [
-    {
-      groupName: "Recommended",
-      wallets: [rainbowWallet, rainbowWeb3AuthConnector, metaMaskWallet],
-    },
-  ],
-});
-
-// console.log("configs", wagmiConfig, config);
+import { wagmiConfig } from "~~/services/web3/wagmiConfig";
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   useInitializeNativeCurrencyPrice();
@@ -69,7 +46,7 @@ export const ScaffoldEthAppWithProviders = ({ children }: { children: React.Reac
   }, []);
 
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <ProgressBar />
         <RainbowKitProvider

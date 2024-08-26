c8d07d398b30d0d205ba8fbbbaa23bca681e8b79
t0xny <78578020+tonynacumoto@users.noreply.github.com>
2024-08-26 00:40:39 -0600

    adds main landing page

---
+++ packages/nextjs/components/Text.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable prettier/prettier */
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { Text, TextProps } from '@chakra-ui/react'
+
+interface Props extends TextProps {
+  className?: string;
+  children: any;
+  id?: string;
+  highlight?: string;
+  tiny?: boolean;
+  ellipsis?: boolean;
+  large?: boolean;
+  xl?: boolean;
+  xs?: boolean;
+  bold?: boolean;
+  color?: string;
+}
+
+export const StyledSpan = styled.span<Props>`
+  position: relative;
+  ${({ highlight }) =>
+    `&:after {
+        content: '';
+        width: calc(100% - 3px);
+        height: 40%;
+        top: 60%;
+        left: 5px;
+        right: 0px;
+        position: absolute;
+        opacity: 0.3;
+        background-color: var(--chakra-colors-${highlight}Brand)
+      `}
+`;
+
+const ExportText: FC<Props> = ({ className = '', children, highlight, tiny, bold, size = 'sm', ellipsis, ...props }) => {
+  return (
+    <Text
+      {...props}
+      as={props.as || 'span'}
+      className={`whitespace-pre-line
+          ${tiny ? 'text-xs text-gray-400' : ''}
+          ${bold ? 'font-bold' : ''}
+          ${size ? `text-${size}` : ''}
+          ${ellipsis ? ` text-ellipsis overflow-hidden ` : ''}
+        ${className} `.replace(/(\r\n|\n|\r)/gm, "").trim()}
+    >
+      {highlight ? <StyledSpan highlight={highlight}>{children}</StyledSpan> : children}
+    </Text>
+  );
+};
+
+export default ExportText;

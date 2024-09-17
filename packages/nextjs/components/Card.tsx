1c28aa1b5852694fad2d8e91a4f8ff7eefde834c
t0xny <78578020+tonynacumoto@users.noreply.github.com>
2024-09-16 20:11:10 -0600

    some style tweaks

--- packages/nextjs/components/Card.tsx
+++ packages/nextjs/components/Card.tsx
@@ -17,19 +17,40 @@ const StyledCard = styled.div`
   }
 `;
 
+const ImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  padding-top: 100%; // 16:9 aspect ratio is 56.25%
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
 const CustomCard: React.FC<CardProps> = ({ className, children, title, footer, align = "left", imageUrl }) => {
   return (
     <StyledCard className={`${className + " card bg-neutral text-neutral-content rounded-lg overflow-hidden"}`}>
-      {imageUrl && <Image src={imageUrl} alt={"nft display"} />}
-      <div className={`card-body items-${align}`}>
+      {imageUrl && (
+        <ImageWrapper>
+          <Image src={imageUrl} alt={"nft display"} />
+        </ImageWrapper>
+      )}
+      <div className={`card-body items-${align} flex flex-col justify-between`}>
         {title && <h1 className="card-title">{title}</h1>}
         {children}
-        {footer && (
-          <div className="card-actions justify-end">
-            <div className="divider w-full"></div>
-            {footer}
-          </div>
-        )}
+        <div className="flex flex-col justify-between">
+          {footer && (
+            <div className="card-actions justify-end">
+              <div className="divider w-full"></div>
+              {footer}
+            </div>
+          )}
+        </div>
       </div>
     </StyledCard>
   );
